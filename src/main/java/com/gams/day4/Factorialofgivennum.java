4e067e6306ccd50abdb18690f35bee897bd36350
Your Name <you@example.com>
2025-03-09 13:23:18 +0530

    added boxes required and DiscountCalculator

---
+++ src/main/java/com/gams/day4/Factorialofgivennum.java
@@ -0,0 +1,16 @@
+package com.gams.day4;
+
+import java.util.Scanner;
+
+public class Factorialofgivennum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=5;
+        int num=1;
+        for (int i =1; i<=n; i++) {
+            num*= i;
+        }
+        System.out.println("factorial of the given num"+n+" is "+num+"");
+    }
+
+}
