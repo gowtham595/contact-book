77e9dd58cf535e166b8dd3ede5958d5192f77e48
Your Name <you@example.com>
2025-03-11 07:50:02 +0530

    factors of the givem num and its count

---
+++ src/main/java/com/gams/day4/FactorsofTheGivennum.java
@@ -0,0 +1,19 @@
+package com.gams.day4;
+
+import java.util.Scanner;
+
+public class FactorsofTheGivennum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = 0;
+        int num=18;
+        for (int i =1; i<=num; i++) {
+            if (num % i == 0){
+                System.out.println(i+"");
+                count++;
+            }
+        }
+        System.out.println("factors count of "+num+":"+count);
+    }
+
+}
